66ed1419d85305b4ad8fc2ccd907443640f0868f
Yassine2002FS <yassinemalhaoui@icloud.com>
2024-04-23 16:01:06 +0200

    Oefening 01

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -3,11 +3,12 @@
 * Verander in de code online alle "var" in "let".
 */
 
+let map = L.map('map').setView([51.505, -0.09], 13);
 
-
-
-
-
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
 
